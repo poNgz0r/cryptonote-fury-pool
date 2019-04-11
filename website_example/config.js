6e2dfbc99e7542329c7d4ae4707aaef30bc011b5
poNgz0r <dennis.kallansee@live.nl>
2019-04-11 23:00:54 +0200

    Added FuryCoin website example
    Added website example v1

---
+++ website_example/config.js
@@ -0,0 +1,13 @@
+var api = "https://YOUR_POOL_URL:2888";
+
+var email = null;
+var telegram = null;
+var discord = "https://discord.gg/ZfrPMby";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "https://fury-block-explorer.piratepools.nl/block/{id}";
+var transactionExplorer = "https://fury-block-explorer.piratepools.nl/tx/{id}";
+
+var themeCss = "themes/stellite.css";
+var defaultLang = 'en';
